11387ef8eb51c33f6c6c641e3a13d2991d26d21f
Rafael Romão <rafaelromao@gmail.com>
2022-02-01 13:09:25 -0300

    Update dynamic_macro.c

--- qmk/users/rafaelromao/features/dynamic_macro.c
+++ qmk/users/rafaelromao/features/dynamic_macro.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 Rafael Romão @rafaelromao
+/* Copyright 2022 Rafael Romão @rafaelromao
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
