46e276385ed7224f27ec4f7e54b5e8ab97896447
Rafael Romão <rafaelromao@gmail.com>
2022-02-14 16:07:05 -0300

    adjust tap hold config

--- src/qmk/users/rafaelromao/features/taphold.c
+++ src/qmk/users/rafaelromao/features/taphold.c
@@ -26,7 +26,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LOW_SPC:
         case RAI_BSP:
-            return TAPPING_TERM - 40;
+            return TAPPING_TERM + 40;
         default:
             return TAPPING_TERM;
     }
