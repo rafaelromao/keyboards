947fed6b9a6915d0d6aaafd7aceef54a8e80763e
Rafael Romão <rafaelromao@gmail.com>
2022-05-04 11:54:39 -0300

    refactoring

--- src/qmk/users/rafaelromao/features/custom_shift.c
+++ src/qmk/users/rafaelromao/features/custom_shift.c
@@ -24,28 +24,6 @@ process_record_result_t process_custom_shift(uint16_t keycode, keyrecord_t *reco
                 return PROCESS_RECORD_RETURN_FALSE;
             }
             return PROCESS_RECORD_CONTINUE;
-
-            // Shift+Leader = comma
-
-        case KC_LEAD:
-            if (get_mods() & MOD_MASK_SHIFT) {
-                unregister_mods(MOD_LSFT);
-                tap_code(KC_COMM);
-                register_mods(MOD_LSFT);
-                return PROCESS_RECORD_RETURN_FALSE;
-            }
-            return PROCESS_RECORD_CONTINUE;
-
-            //     // Shift+../ = dot
-
-            // case MC_DDS:
-            //     if (get_mods() & MOD_MASK_SHIFT) {
-            //         unregister_mods(MOD_LSFT);
-            //         tap_code(KC_DOT);
-            //         register_mods(MOD_LSFT);
-            //         return PROCESS_RECORD_RETURN_FALSE;
-            //     }
-            //     return PROCESS_RECORD_CONTINUE;
     }
 
     switch (key) {
