83312cadc959b95d396d43df3523a89f6c3cd43b
Rafael Romão <rafaelromao@gmail.com>
2022-12-18 10:07:42 -0300

    adjust rgb

--- src/qmk/keyboards/kprepublic/bm40hsrgb/keymaps/rafaelromao/keymap.c
+++ src/qmk/keyboards/kprepublic/bm40hsrgb/keymaps/rafaelromao/keymap.c
@@ -155,7 +155,7 @@ void set_rgblight_by_layer(uint32_t layer) {
                 rgb_matrix_set_color(leds[i], RGB_RED);
                 break;
             default:
-                rgb_matrix_set_color(leds[i], RGB_SPRINGGREEN);
+                rgb_matrix_set_color(leds[i], RGB_BLUE);
                 break;
         }
     }
@@ -186,7 +186,7 @@ void rgb_matrix_indicators_user(void) {
         } else if (has_any_smart_case()) {
             rgb_matrix_set_color(leds[i], RGB_YELLOW);
         } else if (isShift) {
-            rgb_matrix_set_color(leds[i], RGB_TEAL);
+            rgb_matrix_set_color(leds[i], RGB_BLUE);
         } else if (isCtrl || isAlt || isGui) {
             rgb_matrix_set_color(leds[i], RGB_WHITE);
         } else {
