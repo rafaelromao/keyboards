99849106b70ff9d30cca738b39c30bbc42c25d61
Rafael Romão <rafaelromao@gmail.com>
2022-12-27 17:36:40 -0300

    improve selectword and selectline

--- src/qmk/users/rafaelromao/features/select_word.c
+++ src/qmk/users/rafaelromao/features/select_word.c
@@ -5,13 +5,11 @@ extern os_t os;
 select_word_t select_word = {.state = STATE_NONE};
 
 process_record_result_t process_select_word(uint16_t keycode, keyrecord_t* record) {
+    bool isShifted = get_mods() & MOD_MASK_SHIFT || get_oneshot_mods() & MOD_MASK_SHIFT ||
+                     get_oneshot_locked_mods() & MOD_MASK_SHIFT;
     if ((keycode == MC_SELW || keycode == MC_SELL) && record->event.pressed) {
-        bool isShifted = get_mods() & MOD_MASK_SHIFT || get_oneshot_mods() & MOD_MASK_SHIFT ||
-                         get_oneshot_locked_mods() & MOD_MASK_SHIFT;
-        if (keycode == MC_SELL || isShifted || select_word.state == STATE_LINE_SELECTED) {
+        if (keycode == MC_SELL || select_word.state == STATE_LINE_SELECTED) {
             // Select Line
-            clear_mods();
-            clear_locked_and_oneshot_mods();
             if (select_word.state == STATE_NONE) {
                 tap_code(KC_HOME);
                 wait_ms(30);
@@ -20,7 +18,11 @@ process_record_result_t process_select_word(uint16_t keycode, keyrecord_t* recor
                 select_word.state = STATE_FIRST_LINE;
             } else {
                 register_mods(MOD_LSFT);
-                register_code(KC_DOWN);
+                if (isShifted) {
+                    register_code(KC_UP);
+                } else {
+                    register_code(KC_DOWN);
+                }
                 register_code(KC_END);
                 select_word.state = STATE_LINE;
             }
@@ -32,11 +34,11 @@ process_record_result_t process_select_word(uint16_t keycode, keyrecord_t* recor
                 register_code(KC_LCTL);
             }
             if (select_word.state == STATE_NONE) {
-                tap_code(KC_RIGHT);
                 tap_code(KC_LEFT);
+                tap_code(KC_RIGHT);
             }
             register_mods(MOD_LSFT);
-            register_code(KC_RGHT);
+            register_code(KC_LEFT);
             select_word.state = STATE_WORD;
         }
         return PROCESS_RECORD_RETURN_FALSE;
@@ -44,7 +46,7 @@ process_record_result_t process_select_word(uint16_t keycode, keyrecord_t* recor
 
     switch (select_word.state) {
         case STATE_WORD:
-            unregister_code(KC_RGHT);
+            unregister_code(KC_LEFT);
             unregister_mods(MOD_LSFT);
             if (os.type == MACOS) {
                 unregister_code(KC_LALT);
@@ -61,12 +63,22 @@ process_record_result_t process_select_word(uint16_t keycode, keyrecord_t* recor
 
         case STATE_LINE:
             unregister_mods(MOD_LSFT);
-            unregister_code(KC_DOWN);
+            if (isShifted) {
+                unregister_code(KC_UP);
+            } else {
+                unregister_code(KC_DOWN);
+            }
             unregister_code(KC_END);
             select_word.state = STATE_LINE_SELECTED;
             break;
 
         case STATE_WORD_SELECTED:
+            if (keycode != MC_SELW && record->event.pressed) {
+                unregister_mods(MOD_LSFT);
+                tap_code(KC_LEFT);
+                select_word.state = STATE_NONE;
+                return PROCESS_RECORD_CONTINUE;
+            }
         case STATE_LINE_SELECTED:
             if (keycode != MC_SELW && record->event.pressed) {
                 unregister_mods(MOD_LSFT);
