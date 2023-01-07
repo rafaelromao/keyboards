a9a56c38997ed9bf09f98d8d00a5bc8a90c5183d
Rafael Romão <rafaelromao@gmail.com>
2023-01-07 11:36:19 -0300

    adjust mj64

--- src/qmk/keyboards/melgeek/mj64/keymaps/clopes.le/keymap.c
+++ src/qmk/keyboards/melgeek/mj64/keymaps/clopes.le/keymap.c
@@ -25,14 +25,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		    KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     BR_ACUT,  TD_LBRC, BR_BSLS,
 			KC_CAPS,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     BR_CCED,  BR_TILD,  KC_ENT,
 		    KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   BR_SCLN,  SF_SLSH,  KC_UP,   KC_DEL,
-		    KC_LCTL,   KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  MO(1),    KC_LEFT,  KC_DOWN, KC_RIGHT
+		    KC_LCTL,   KC_LGUI,  KC_LALT,                      LT(1, KC_SPC),                          KC_RALT,  MO(1),    KC_LEFT,  KC_DOWN, KC_RIGHT
 	),
 	[1] = LAYOUT_64_ansi( /* FN */
 			KC_ESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,
 			_______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  BR_RBRC,  _______,
-            _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+            _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_F9,
 		    _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_SLSH,  KC_PGUP,  KC_INS,
-			_______,   MO(2),    _______,                      _______,                                _______,  _______,  KC_HOME,  KC_PGDN,  KC_END
+			_______,   MO(2),    _______,                      KC_SPC,                                 _______,  _______,  KC_HOME,  KC_PGDN,  KC_END
 	),
 	[2] = LAYOUT_64_ansi( /* Adjust */
 			_______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
