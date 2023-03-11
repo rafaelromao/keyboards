06cc3f1e0dff647ddf18a03887e10f0bdee242df
Rafael Romão <rafaelromao@gmail.com>
2023-03-11 11:25:15 -0300

    make oled compile

--- src/qmk/keyboards/handwired/rafaelromao/testpad/config.yd.oled.h
+++ src/qmk/keyboards/handwired/rafaelromao/testpad/config.yd.oled.h
@@ -6,3 +6,8 @@
 #pragma once
 
 #define DEBOUNCE 5
+
+#define OLED_DISPLAY_128X32
+#define I2C1_SCL_PIN GP3
+#define I2C1_SDA_PIN GP2
+#define OLED_BRIGHTNESS 12
---
+++ src/qmk/keyboards/handwired/rafaelromao/testpad/rules.yd.oled.mk
@@ -0,0 +1,6 @@
+# This file intentionally left blank
+
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+
+OLED_DRIVER = SSD1306
