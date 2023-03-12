56bf071a5497fb3728c63e851ae93896b90a211d
Rafael Romão <rafaelromao@gmail.com>
2023-03-12 18:59:32 -0300

    adjust mj64

--- src/qmk/keyboards/melgeek/mj64/keymaps/clopes.le/keymap.c
+++ src/qmk/keyboards/melgeek/mj64/keymaps/clopes.le/keymap.c
@@ -21,17 +21,17 @@
 // clang-format off
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_64_ansi( /* Base */
+	[0] = LAYOUT_64_ansi(
 			TD_ESC,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,
 		    KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     BR_ACUT,  TD_LBRC, BR_BSLS,
-			OS_MAC,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     BR_CCED,  BR_TILD,  KC_ENT,
+			KC_CAPS,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     BR_CCED,  BR_TILD,  KC_ENT,
 		    KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   BR_SCLN,  SF_SLSH,  KC_UP,   KC_DEL,
 		    KC_LCTL,   KC_LGUI,  KC_LALT,                      LT(1, KC_SPC),                          OS_MAC,   MO(1),    KC_LEFT,  KC_DOWN, KC_RIGHT
 	),
-	[1] = LAYOUT_64_ansi( /* FN */
+	[1] = LAYOUT_64_ansi(
 			KC_ESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,
 			_______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  BR_RBRC,  _______,
-            KC_CAPS,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_F9,
+            _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_F9,
 		    _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_SLSH,  KC_PGUP,  KC_INS,
 			_______,   MO(3),    _______,                      KC_SPC,                                 _______,  _______,  KC_HOME,  KC_PGDN,  KC_END
 	),
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		    _______,   _______,  _______,  MC_CONF,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
 			_______,   _______,  _______,                      _______,                                _______,  _______,  _______,  _______,  _______
     ),
-	[3] = LAYOUT_64_ansi( /* Adjust */
+	[3] = LAYOUT_64_ansi(
 			_______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
 			_______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
             NK_TOGG,   RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  RGB_SPI,  RGB_SPD,  _______,  _______,
