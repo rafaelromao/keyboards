0099bea980b0aca63040e82de03e1631bf71656f
Rafael Romão <rafaelromao@gmail.com>
2023-03-20 15:08:51 -0300

    fix hummingbird definitions

--- src/qmk/keyboards/handwired/rafaelromao/hummingbird/rules.mk
+++ src/qmk/keyboards/handwired/rafaelromao/hummingbird/rules.mk
@@ -1,5 +1,1 @@
-# This file intentionally left blank
-
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 WS2812_DRIVER = vendor
