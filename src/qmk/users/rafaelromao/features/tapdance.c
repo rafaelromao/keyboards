434cfda81449bae4993f479439ee3c908ad34685
Rafael Romão <rafaelromao@gmail.com>
2023-05-10 13:56:35 -0300

    fix /\?

--- src/qmk/users/rafaelromao/features/tapdance.c
+++ src/qmk/users/rafaelromao/features/tapdance.c
@@ -520,7 +520,13 @@ void td_slash(tap_dance_state_t *state, void *user_data) {
             tap_code(KC_SLSH);
             break;
         case TD_SINGLE_HOLD:
-            tap_code16(KC_BSLS);
+            if (!is_shifted()) {
+                tap_code16(KC_BSLS);
+            } else {
+                clear_shift();
+                tap_code16(KC_END);
+                tap_code16(KC_QUES);
+            }
             break;
         default:
             break;
