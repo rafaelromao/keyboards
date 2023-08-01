e6d6ac6c273eda6d0df96c21d557fc7afab0e0fe
Rafael Romão <rafaelromao@gmail.com>
2023-08-01 08:54:39 -0300

    simplify build script for zen

--- init_zmk.sh
+++ init_zmk.sh
@@ -43,7 +43,8 @@ build_zen_left="west build -s app -b corneish_zen_v2_left --build-dir build/corn
 build_zen_right="west build -s app -b corneish_zen_v2_right --build-dir build/corneish_zen_right -- -DZMK_CONFIG=$KEYBOARD_HOME/src/zmk/config/boards/lowprokb.ca/corneish-zen"
 archive_zen_left="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/corneish_zen_left/zephyr/zmk.uf2 ] && mv build/corneish_zen_left/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/corneish_zen_v2_left-zmk.uf2"
 archive_zen_right="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/corneish_zen_right/zephyr/zmk.uf2 ] && mv build/corneish_zen_right/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/corneish_zen_v2_right-zmk.uf2"
-alias build_zen="cd zmk && ${build_zen_left} && ${archive_zen_left} && ${build_zen_right} && ${archive_zen_right} && cd .."
+alias build_zen_both_sides="cd zmk && ${build_zen_left} && ${archive_zen_left} && ${build_zen_right} && ${archive_zen_right} && cd .."
+alias build_zen="cd zmk && ${build_zen_left} && ${archive_zen_left} && cd .."
 
 echo "Creating Hummingbird build alias..."
 build_hummingbird_unibody="west build --pristine -s app -b seeeduino_xiao_ble --build-dir build/hummingbird -- -DSHIELD='hw_hummingbird rgbled_widget' -DZMK_CONFIG=$KEYBOARD_HOME/src/zmk/config/boards/handwired"
