c3a7e11463d237be5cc7976231e96239bcfbdd19
Rafael Romão <rafaelromao@gmail.com>
2024-03-28 11:19:22 -0300

    update zmk

--- init_zmk.sh
+++ init_zmk.sh
@@ -25,7 +25,7 @@ git submodule update --init --recursive --progress
 echo "Checking out zmk..."
 cd $ZMK_HOME
 git fetch
-git checkout 20240212/rafaelromao/main
+git checkout 20240328/rafaelromao/main
 git pull
 cd $KEYBOARD_HOME
 
