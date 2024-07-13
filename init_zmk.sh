470816172d2788e08b25f8777aa712b36672ce22
Rafael Romão <rafaelromao@gmail.com>
2024-07-13 15:35:36 -0300

    Diamond (#20)
    * add diamond with dongle module
    
    * add central_left
    
    * fix diamond pinout
    
    * add diamond images

--- init_zmk.sh
+++ init_zmk.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 KEYBOARD_HOME="$(pwd)"
-export ZMK_HOME="$KEYBOARD_HOME/modules/rafaelromao/zmk"
-RGBWIDGET_HOME="$KEYBOARD_HOME/modules/caksoylar/zmk-rgbled-widget"
+ZMK_MODULE="modules/rafaelromao/zmk"
+RGBWIDGET_MODULE="modules/caksoylar/zmk-rgbled-widget"
+DONGLE_MODULE="modules/englmaxi/zmk-dongle-display"
+export ZMK_HOME="$KEYBOARD_HOME/$ZMK_MODULE"
+RGBWIDGET_HOME="$KEYBOARD_HOME/$RGBWIDGET_MODULE"
+DONGLE_HOME="$KEYBOARD_HOME/$DONGLE_MODULE"
+
+if [[ ! -d "$DONGLE_HOME" ]]
+then
+    echo "Add git sub-modules..."
+    git submodule add -f https://github.com/englmaxi/zmk-dongle-display $DONGLE_MODULE
+fi
 
 if [[ ! -d "$RGBWIDGET_HOME" ]]
 then
     echo "Add git sub-modules..."
-    git submodule add -f https://github.com/caksoylar/zmk-rgbled-widget modules/caksoylar/zmk-rgbled-widget
+    git submodule add -f https://github.com/caksoylar/zmk-rgbled-widget $RGBWIDGET_MODULE
 fi
 
 INIT=false
@@ -15,7 +25,7 @@ if [[ ! -d "$ZMK_HOME/.west" ]]
 then
     INIT=true
     echo "Add git sub-modules..."
-    git submodule add -f https://github.com/rafaelromao/zmk modules/rafaelromao/zmk
+    git submodule add -f https://github.com/rafaelromao/zmk $ZMK_MODULE
 fi
 
 echo "Update git sub-modules..."
@@ -46,6 +56,11 @@ unset GNUARMEMB_TOOLCHAIN_PATH
 export ZEPHYR_SDK_INSTALL_DIR=~/zephyr-sdk-0.16.3
 cd $KEYBOARD_HOME
 
+echo "Creating Testpad build alias..."
+build_testpad_unibody="west build -s app -b seeeduino_xiao_ble --build-dir build/testpad -- -DSHIELD='testpad rgbled_adapter' -DZMK_CONFIG=$KEYBOARD_HOME/src/zmk/boards/handwired -DZMK_EXTRA_MODULES=$RGBWIDGET_HOME"
+archive_testpad_unibody="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/testpad/zephyr/zmk.uf2 ] && mv build/testpad/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/testpad-zmk.uf2"
+alias build_testpad="cd ${ZMK_HOME} && ${build_testpad_unibody} && ${archive_testpad_unibody} && cd $KEYBOARD_HOME"
+
 echo "Creating Reset build alias..."
 build_reset_unibody="west build -s app -b nice_nano_v2 --build-dir build/reset -- -DSHIELD='settings_reset'"
 archive_reset_unibody="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/reset/zephyr/zmk.uf2 ] && mv build/reset/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/reset-zmk.uf2"
@@ -56,7 +71,7 @@ build_zen_left="west build -s app -b corneish_zen_v2_left --build-dir build/corn
 build_zen_right="west build -s app -b corneish_zen_v2_right --build-dir build/corneish_zen_right -- -DZMK_CONFIG=$KEYBOARD_HOME/src/zmk/boards/lowprokb.ca/corneish-zen"
 archive_zen_left="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/corneish_zen_left/zephyr/zmk.uf2 ] && mv build/corneish_zen_left/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/corneish_zen_v2_left-zmk.uf2"
 archive_zen_right="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/corneish_zen_right/zephyr/zmk.uf2 ] && mv build/corneish_zen_right/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/corneish_zen_v2_right-zmk.uf2"
-alias build_zen_both_sides="cd ${ZMK_HOME} && ${build_zen_left} && ${archive_zen_left} && ${build_zen_right} && ${archive_zen_right} && cd ${KEYBOARD_HOME}"
+alias build_zen_all="cd ${ZMK_HOME} && ${build_zen_left} && ${archive_zen_left} && ${build_zen_right} && ${archive_zen_right} && cd ${KEYBOARD_HOME}"
 alias build_zen="cd ${ZMK_HOME} && ${build_zen_left} && ${archive_zen_left} && cd ${KEYBOARD_HOME}"
 
 echo "Creating Rommana build alias..."
@@ -64,15 +79,22 @@ build_rommana_unibody="west build -s app -b seeeduino_xiao_rp2040 --build-dir bu
 archive_rommana_unibody="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/rommana/zephyr/zmk.uf2 ] && mv build/rommana/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/rommana-zmk.uf2"
 alias build_rommana="cd ${ZMK_HOME} && ${build_rommana_unibody} && ${archive_rommana_unibody} && cd $KEYBOARD_HOME"
 
-echo "Creating Cygnus alias..."
+echo "Creating Cygnus build alias..."
 build_cygnus_left="west build -s app -b nice_nano_v2 --build-dir build/cygnus_left -- -DSHIELD='cygnus_left' -DZMK_CONFIG=$KEYBOARD_HOME/src/zmk/boards/handwired"
 build_cygnus_right="west build -s app -b nice_nano_v2 --build-dir build/cygnus_right -- -DSHIELD='cygnus_right' -DZMK_CONFIG=$KEYBOARD_HOME/src/zmk/boards/handwired"
 archive_cygnus_left="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/cygnus_left/zephyr/zmk.uf2 ] && mv build/cygnus_left/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/cygnus_left-zmk.uf2"
 archive_cygnus_right="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/cygnus_right/zephyr/zmk.uf2 ] && mv build/cygnus_right/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/cygnus_right-zmk.uf2"
-alias build_cygnus_both_sides="cd ${ZMK_HOME} && ${build_cygnus_left} && ${archive_cygnus_left} && ${build_cygnus_right} && ${archive_cygnus_right} && cd ${KEYBOARD_HOME}"
+alias build_cygnus_all="cd ${ZMK_HOME} && ${build_cygnus_left} && ${archive_cygnus_left} && ${build_cygnus_right} && ${archive_cygnus_right} && cd ${KEYBOARD_HOME}"
 alias build_cygnus="cd ${ZMK_HOME} && ${build_cygnus_left} && ${archive_cygnus_left} && cd ${KEYBOARD_HOME}"
 
-echo "Creating Testpad build alias..."
-build_testpad_unibody="west build -s app -b seeeduino_xiao_ble --build-dir build/testpad -- -DSHIELD='testpad rgbled_adapter' -DZMK_CONFIG=$KEYBOARD_HOME/src/zmk/boards/handwired -DZMK_EXTRA_MODULES=$RGBWIDGET_HOME"
-archive_testpad_unibody="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/testpad/zephyr/zmk.uf2 ] && mv build/testpad/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/testpad-zmk.uf2"
-alias build_testpad="cd ${ZMK_HOME} && ${build_testpad_unibody} && ${archive_testpad_unibody} && cd $KEYBOARD_HOME"
+echo "Creating Diamond build alias..."
+build_diamond_central_dongle="west build -s app -b nice_nano_v2 --build-dir build/diamond_central_dongle -- -DSHIELD='diamond_central_dongle dongle_display' -DZMK_CONFIG=$KEYBOARD_HOME/src/zmk/boards/handwired -DZMK_EXTRA_MODULES=$DONGLE_HOME"
+build_diamond_central_left="west build -s app -b nice_nano_v2 --build-dir build/diamond_central_left -- -DSHIELD='diamond_central_left' -DZMK_CONFIG=$KEYBOARD_HOME/src/zmk/boards/handwired"
+build_diamond_peripheral_left="west build -s app -b nice_nano_v2 --build-dir build/diamond_peripheral_left -- -DSHIELD='diamond_peripheral_left' -DZMK_CONFIG=$KEYBOARD_HOME/src/zmk/boards/handwired"
+build_diamond_peripheral_right="west build -s app -b nice_nano_v2 --build-dir build/diamond_peripheral_right -- -DSHIELD='diamond_peripheral_right' -DZMK_CONFIG=$KEYBOARD_HOME/src/zmk/boards/handwired"
+archive_diamond_central_dongle="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/diamond_central_dongle/zephyr/zmk.uf2 ] && mv build/diamond_central_dongle/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/diamond_central_dongle-zmk.uf2"
+archive_diamond_central_left="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/diamond_central_left/zephyr/zmk.uf2 ] && mv build/diamond_central_left/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/diamond_central_left-zmk.uf2"
+archive_diamond_peripheral_left="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/diamond_peripheral_left/zephyr/zmk.uf2 ] && mv build/diamond_peripheral_left/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/diamond_peripheral_left-zmk.uf2"
+archive_diamond_peripheral_right="mkdir -p $KEYBOARD_HOME/build/artifacts; [ -f build/diamond_peripheral_right/zephyr/zmk.uf2 ] && mv build/diamond_peripheral_right/zephyr/zmk.uf2 $KEYBOARD_HOME/build/artifacts/diamond_peripheral_right-zmk.uf2"
+alias build_diamond_all="cd ${ZMK_HOME} && ${build_diamond_central_left} && ${archive_diamond_central_left} && ${build_diamond_central_dongle} && ${archive_diamond_central_dongle} && ${build_diamond_peripheral_left} && ${archive_diamond_peripheral_left} && ${build_diamond_peripheral_right} && ${archive_diamond_peripheral_right} && cd ${KEYBOARD_HOME}"
+alias build_diamond="cd ${ZMK_HOME} && ${build_diamond_central_dongle} && ${archive_diamond_central_dongle} && ${build_diamond_central_left} && ${archive_diamond_central_left} && cd $KEYBOARD_HOME"
---
+++ src/zmk/boards/handwired/boards/shields/diamond/readme.md
@@ -0,0 +1,44 @@
+## Diamond
+### A 24 keys handwired split keyboard.
+
+### Build Details
+
+Diamond is a handwired board originally designed by me but heavily inspired by the [Rommana](https://github.com/AlaaSaadAbdo/Rommana) and using the switch holders of the [Dactyl Cygnus](https://github.com/juhakaup/keyboards).
+
+### Diamond
+![img](../../../../../../../img/diamond.jpeg)
+
+Parts:
+- 24 MX switches (Boba U4).
+- 18 1u MX keycaps (MT3 Blanks).
+- 6 1.5u MX keycaps (MT3 Blanks).
+- Diamond [case](../../../../../../../stls/diamond).
+- [3 Supermini NRF52840](https://www.aliexpress.com/item/1005006035267231.html).
+- [3 500mAh 3.7v battery - 752035](https://www.aliexpress.com/item/1005005984848543.html)
+- [24 Single Switch PCBs](https://keycapsss.com/keyboard-parts/pcbs/173/mxledbit-single-switch-pcb-mx-choc-hot-swap-socket).
+- [Dupont Cables](https://aliexpress.com/item/1005004155181609.html).
+- [28 AWG Wires](https://www.aliexpress.com/item/1005006249194665.html).
+- [1n4148 diodes + diode legs](https://pt.aliexpress.com/item/1005003540554760.html).
+
+### ZMK
+
+#### Compile
+
+- Run the script `build_diamond` to build the firmware for the central left side (use it without a dongle) and the dongle only.
+- Run the script `build_diamond_all` to build the firmware for both sides and the dongle.
+
+#### Flash
+
+- Connect the central dongle, put it in bootloader mode and copy the file `build/artifacts/diamond_dongle-zmk.uf2` to the drive NICENANO. Wait for the drive to be disconnected.
+- Connect the left half, put it in bootloader mode and copy the file `build/artifacts/diamond_left-zmk.uf2` to the drive NICENANO. Wait for the drive to be disconnected.
+- Connect the right half, put it in bootloader mode and copy the file `build/artifacts/diamond_right-zmk.uf2` to the drive NICENANO. Wait for the drive to be disconnected.
+
+## Keymap
+
+- The list of supported features in this keymap can be found [here](../../../../../readme.md).
+
+## Resources
+
+- [Home](https://github.com/rafaelromao/keyboards)
+- [ZMK Docs](https://zmk.dev/docs)
+- [Keyboard Tester](https://config.qmk.fm/#/test)
