95cfda2adf37ed41860fd25f0cc3771a1155c53e
Rafael Romão <rafaelromao@gmail.com>
2024-10-26 19:28:26 -0300

    restore working version of the modules

--- src/zmk/definitions/keycodes.h
+++ src/zmk/definitions/keycodes.h
@@ -8,48 +8,45 @@
 
 #define C_CEDIL (LA(ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_C)))
 
-// #define CAPSWORD_TERM 0x070100
+#define CAPSWORD_TERM 0x070100
 
-// #define H_TERM        CAPSWORD_TERM
-// #define V_TERM        CAPSWORD_TERM
+#define QU_TERM       0x070101
+#define AACU_TERM     0x070102
+#define EACU_TERM     0x070103
+#define IACU_TERM     0x070104
+#define OACU_TERM     0x070105
+#define UACU_TERM     0x070106
+#define ACIR_TERM     0x070107
+#define ECIR_TERM     0x070108
+#define OCIR_TERM     0x070109
+#define ATIL_TERM     0x07010a
+#define OTIL_TERM     0x07010b
+#define AGRV_TERM     0x07010c
+#define SQUO_TERM     0x07010d
 
-// #define QU_TERM       CAPSWORD_TERM
-// #define AACU_TERM     CAPSWORD_TERM
-// #define EACU_TERM     CAPSWORD_TERM
-// #define IACU_TERM     CAPSWORD_TERM
-// #define OACU_TERM     CAPSWORD_TERM
-// #define UACU_TERM     CAPSWORD_TERM
-// #define ACIR_TERM     CAPSWORD_TERM
-// #define ECIR_TERM     CAPSWORD_TERM
-// #define OCIR_TERM     CAPSWORD_TERM
-// #define ATIL_TERM     CAPSWORD_TERM
-// #define OTIL_TERM     CAPSWORD_TERM
-// #define AGRV_TERM     CAPSWORD_TERM
-// #define SQUO_TERM     CAPSWORD_TERM
+#define C_QU_TERM     0x07010e
+#define C_AACU_TERM   0x07010f
+#define C_EACU_TERM   0x070110
+#define C_IACU_TERM   0x070111
+#define C_OACU_TERM   0x070112
+#define C_UACU_TERM   0x070113
+#define C_ACIR_TERM   0x070114
+#define C_ECIR_TERM   0x070115
+#define C_OCIR_TERM   0x070116
+#define C_ATIL_TERM   0x070117
+#define C_OTIL_TERM   0x070118
+#define C_AGRV_TERM   0x070119
+#define C_SQUO_TERM   0x07011a
 
-// #define C_QU_TERM     CAPSWORD_TERM
-// #define C_AACU_TERM   CAPSWORD_TERM
-// #define C_EACU_TERM   CAPSWORD_TERM
-// #define C_IACU_TERM   CAPSWORD_TERM
-// #define C_OACU_TERM   CAPSWORD_TERM
-// #define C_UACU_TERM   CAPSWORD_TERM
-// #define C_ACIR_TERM   CAPSWORD_TERM
-// #define C_ECIR_TERM   CAPSWORD_TERM
-// #define C_OCIR_TERM   CAPSWORD_TERM
-// #define C_ATIL_TERM   CAPSWORD_TERM
-// #define C_OTIL_TERM   CAPSWORD_TERM
-// #define C_AGRV_TERM   CAPSWORD_TERM
-// #define C_SQUO_TERM   CAPSWORD_TERM
-
-// #define S_QU_TERM     CAPSWORD_TERM
-// #define S_AACU_TERM   CAPSWORD_TERM
-// #define S_EACU_TERM   CAPSWORD_TERM
-// #define S_IACU_TERM   CAPSWORD_TERM
-// #define S_OACU_TERM   CAPSWORD_TERM
-// #define S_UACU_TERM   CAPSWORD_TERM
-// #define S_ACIR_TERM   CAPSWORD_TERM
-// #define S_ECIR_TERM   CAPSWORD_TERM
-// #define S_OCIR_TERM   CAPSWORD_TERM
-// #define S_ATIL_TERM   CAPSWORD_TERM
-// #define S_OTIL_TERM   CAPSWORD_TERM
-// #define S_AGRV_TERM   CAPSWORD_TERM
+#define S_QU_TERM     0x07011b
+#define S_AACU_TERM   0x07011c
+#define S_EACU_TERM   0x07011d
+#define S_IACU_TERM   0x07011e
+#define S_OACU_TERM   0x070120
+#define S_UACU_TERM   0x070121
+#define S_ACIR_TERM   0x070122
+#define S_ECIR_TERM   0x070123
+#define S_OCIR_TERM   0x070124
+#define S_ATIL_TERM   0x070125
+#define S_OTIL_TERM   0x070126
+#define S_AGRV_TERM   0x070127
