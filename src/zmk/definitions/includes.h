a7aa381c6f4f8fdb91f846cae9900aa2aad2cb91
Rafael Romão <rafaelromao@gmail.com>
2025-02-20 17:20:46 -0300

    refactor multi-os support

---
+++ src/zmk/definitions/includes.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#define MACOS
+
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
+#include <dt-bindings/zmk/mouse.h>
+
+#include "templates.dtsi"
+#include "keycodes.h"
+#include "config.dtsi"
+
+#include "../features/mouse.dtsi"
+
+#include "../features/accents.dtsi"
+#include "../features/shortcuts.dtsi"
+#include "../features/select.dtsi"
+
+#ifdef MACOS
+#include "../features/macos/accents.dtsi"
+#include "../features/macos/shortcuts.dtsi"
+#include "../features/macos/select.dtsi"
+#endif
+#ifdef LINUX
+#include "../features/linux/accents.dtsi"
+#include "../features/linux/shortcuts.dtsi"
+#include "../features/linux/select.dtsi"
+#endif
+#ifdef ANDROID
+#include "../features/android/accents.dtsi"
+#include "../features/android/shortcuts.dtsi"
+#include "../features/android/select.dtsi"
+#endif
+
+#include "../features/smart.dtsi"
+#include "../features/magic.dtsi"
+#include "../features/symbols.dtsi"
+
+#include "../features/combos.dtsi"
+#include "../features/leader.dtsi"
+
+#if __has_include("../secret.dtsi")
+#include "../secret.dtsi"
+#endif
