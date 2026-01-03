489a3f4a207f28391f32acf6590da44ddb726db3
Rafael Romao <rafaelromao@gmail.com>
2026-01-03 11:59:45 -0300

    add vim-mode (wip)

--- scripts/b.sh
+++ scripts/b.sh
@@ -58,6 +58,9 @@ build_dilemma_right() {
 # build lowprokb.ca/corneish-zen -b corneish_zen_v2_left -o LINUX -z caksoylar/zmk -r caksoylar/zen-v1+v2
 # build lowprokb.ca/corneish-zen-with-dongle -b corneish_zen_v2_left -o LINUX -z caksoylar/zmk -r caksoylar/zen-v1+v2
 # build lowprokb.ca/corneish-zen-with-dongle -b corneish_zen_v2_right -o LINUX -z caksoylar/zmk -r caksoylar/zen-v1+v2
+#
+# Building Dilemma peripherals
+# build bastardkb/dilemma r LINUX -m petejohanson/cirque-input-module
 
 # --- Main Script ---
 
