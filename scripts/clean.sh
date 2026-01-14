1fc6b530b79f2353336e4d1c3019182cdb406e36
Rafael Romao <rafaelromao@gmail.com>
2026-01-14 17:48:09 -0300

    rebase

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 rm -rf .west
-rm -rf modules/zmkfirmware/zmk/.west
-rm -rf modules/zmkfirmware/zmk/build
-rm -rf modules/urob
-rm -rf modules/rafaelromao
-rm -rf modules/englmaxi
-rm -rf .git/modules/modules/urob
-rm -rf .git/modules/modules/rafaelromao
-rm -rf .git/modules/modules/englmaxi
+rm -rf modules/*
+rm -rf .git/modules/modules/*
